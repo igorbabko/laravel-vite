f21751dca0f59ccb428e55cad573500c0617b0f7
Igor Babko <i.i.babko@gmail.com>
2024-02-20 12:24:51 +0200

    3.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Laravel / Vite</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body></body>
+</html>
