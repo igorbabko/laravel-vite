b4d989b8817233a88fed9ccc0f53fca0c97e2793
Igor Babko <i.i.babko@gmail.com>
2024-03-09 19:15:27 +0200

    8. Image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import laravel from 'laravel-vite-plugin'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,11 @@ export default defineConfig({
         laravel({
             input: ['resources/js/app.js'],
             refresh: true
+        }),
+        ViteImageOptimizer({
+            jpg: {
+                quality: 50
+            }
         })
     ]
 })
