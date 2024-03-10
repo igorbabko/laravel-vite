c59947b355770eae235fd5399dff8aca25be3286
Igor Babko <i.i.babko@gmail.com>
2024-03-10 09:35:46 +0200

    12. Tailwind CSS installation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 
         @vite('resources/js/app.js')
     </head>
-    <body>
+    <body class="bg-[#14111b] flex flex-col justify-center items-center">
         <div class="images">
             <img src="{{ Vite::asset('resources/img/1.jpg') }}" alt="Image 1">
             <img src="{{ Vite::asset('resources/img/2.jpg') }}" alt="Image 2">
